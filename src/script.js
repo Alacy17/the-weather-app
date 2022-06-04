aa84cf534906837d85b92c8054d559bf02e7f183
Alacy17 <106881384+Alacy17@users.noreply.github.com>
2022-06-04 13:32:46 -0500

    Updated Coding for JS, CSS and HTML
    -Added previous weather app coding for JS, HTML and CSS
    -Changed title to the new correct title of 'The Weather App'
    -Removed the duplicate link strings

---
+++ src/script.js
@@ -0,0 +1,69 @@
+let now = new Date();
+
+let time = now.getTime();
+
+let day = now.getDay();
+
+let hours = now.getHours();
+
+let minutes = now.getMinutes(`2-digit`);
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let weekDay = days[now.getDay()];
+
+let showDate = `${weekDay},${hours}:${minutes}`;
+
+let date = document.querySelector("#current-date");
+date.innerHTML = `${weekDay}, ${hours}:${minutes}`;
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearch);
+
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+  searchCity(city);
+}
+
+function searchCity(city) {
+  let apiKey = "95b866cc0e09746fb0e30ef3ac0e20d0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=95b866cc0e09746fb0e30ef3ac0e20d0&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function searchLocation(position) {
+  let apiKey = "95b866cc0e09746fb0e30ef3ac0e20d0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=95b866cc0e09746fb0e30ef3ac0e20d0&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
+  console.log(apiUrl);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+searchCity("Los Angeles");
