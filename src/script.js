230b180fd2a2fc0e21915705ca7764e904c69a56
Alacy17 <106881384+Alacy17@users.noreply.github.com>
2022-06-12 13:00:45 -0500

    Updated HTML, JS and CSS
    added function for icons to change and updated styling to match

--- src/script.js
+++ src/script.js
@@ -29,6 +29,7 @@ let date = document.querySelector("#current-date");
 date.innerHTML = `${weekDay}, ${hours}:${minutes}`;
 
 function showTemperature(response) {
+  let iconElement = document.querySelector("#icon");
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -39,6 +40,11 @@ function showTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearch);
@@ -59,7 +65,6 @@ function searchLocation(position) {
   let apiKey = "95b866cc0e09746fb0e30ef3ac0e20d0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=95b866cc0e09746fb0e30ef3ac0e20d0&units=imperial`;
   axios.get(apiUrl).then(showTemperature);
-  console.log(apiUrl);
 }
 
 function getCurrentLocation(event) {
