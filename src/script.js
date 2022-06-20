17d19aeb02eb4977d796b4a0e26e3c031bbfb43e
Alacy17 <106881384+Alacy17@users.noreply.github.com>
2022-06-20 13:11:19 -0500

    Updated HTTPS

--- src/script.js
+++ src/script.js
@@ -42,7 +42,7 @@ function showTemperature(response) {
     response.data.weather[0].main;
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
